92b966dd9f0519c35eff3f513f48832bda3cb2ce
Stephan Leroux <stephanleroux@gmail.com>
2012-11-23 17:52:13 -0500

    First commit - setting up maven project

---
+++ postageapp/src/main/java/postageapp/App.java
@@ -0,0 +1,13 @@
+package postageapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
