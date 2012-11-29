aedd6e36179eda18a7fdfcb7432d9dc0dd18de54
Stephan Leroux <stephanleroux@gmail.com>
2012-11-28 19:59:55 -0500

    Updated junit and added some files to begin tests

---
+++ src/test/java/postageapp/ModelTestCase.java
@@ -0,0 +1,12 @@
+package postageapp;
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stephanleroux
+ * Date: 2012-11-28
+ * Time: 7:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ModelTestCase extends TestCase {
+}
---
+++ src/test/java/postageapp/models/AccountInfoTest.java
@@ -0,0 +1,13 @@
+package postageapp.models;
+
+import postageapp.ModelTestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stephanleroux
+ * Date: 2012-11-28
+ * Time: 7:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountInfoTest extends ModelTestCase {
+}
